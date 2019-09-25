2e4f435538845c65b7db0fc5b8a0bfeb8a021b3f
elizabethdaly <liz.m.a.daly@gmail.com>
2019-09-25 12:04:22 +0100

    Change x axis range for power calculation

--- powers.py
+++ powers.py
@@ -4,7 +4,7 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
-x = np.arange(1.0, 10.0, 0.1)
+x = np.arange(1.0, 12.0, 0.1)
 
 plt.plot(x, x**2, 'g-', label='x^2')
 plt.plot(x, x**3, 'b-', label='x^3')
